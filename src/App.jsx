1edfbbf4caa4a873371a8d3b9ce18e328849ea41
bukhtyar <bukhtyar.haider1@gmail.com>
2023-05-15 22:40:58 +0500

    integrate Login container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import "./App.css";
+import { Login } from "./container/Login";
 function App() {
-  return <></>;
+  return (
+    <div className="container">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
