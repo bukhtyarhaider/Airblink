48c068b85672f3c097f11bf09f41ff4d72e4b9b1
bukhtyar <bukhtyar.haider1@gmail.com>
2023-05-15 20:29:44 +0500

    setup firebase

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCW-5qHRoeV0N2A17My_jEzolkI3nvd92o",
+  authDomain: "airblink-assessment.firebaseapp.com",
+  projectId: "airblink-assessment",
+  storageBucket: "airblink-assessment.appspot.com",
+  messagingSenderId: "554158840773",
+  appId: "1:554158840773:web:93afae4e8759b71cb409ac",
+};
+
+const app = initializeApp(firebaseConfig);
