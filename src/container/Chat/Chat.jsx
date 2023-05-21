c98102484ca54d235f2877c51b8c74b0f99058bb
bukhtyar <bukhtyar.haider1@gmail.com>
2023-05-21 16:31:34 +0500

    develop Chat container and integrate all components to Chat container

---
+++ src/container/Chat/Chat.jsx
@@ -0,0 +1,44 @@
+import { signOut, getAuth } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { app, getUsers } from "../../firebase/Firebase";
+import { Profile } from "../../Components/Profile/Profile";
+
+import "./Chat.css";
+import { FriendList } from "../../Components/FriendList/FriendList";
+import { ChatRoom } from "../../Components/ChatRoom/ChatRoom";
+
+export const Chat = ({ user }) => {
+  const auth = getAuth(app);
+  const [users, setUsers] = useState([{ id: "loading..." }]);
+  const [activeUser, setActiveUser] = useState(null);
+
+  const handleUserSelect = (user) => {
+    setActiveUser(user);
+  };
+
+  const onLogout = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {
+        console.log("Logout error:", error);
+      });
+  };
+
+  useEffect(() => {
+    getUsers(user.uid).then((Response) => {
+      return setUsers(Response);
+    });
+  }, [user]);
+
+  return (
+    <div className="chat_container">
+      <div className="chat_left_container">
+        <Profile user={user} logout={onLogout} />
+        <FriendList users={users} onUserSelect={handleUserSelect} />
+      </div>
+      <div className="chat_right_container">
+        <ChatRoom user={user} selectedUser={activeUser} />
+      </div>
+    </div>
+  );
+};
