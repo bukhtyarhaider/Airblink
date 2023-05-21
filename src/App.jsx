fdf3f1a48b5fea87c269179f7b1416e104085719
bukhtyar <bukhtyar.haider1@gmail.com>
2023-05-21 16:33:06 +0500

    fixed: keep  user login on reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { onAuthStateChanged, getAuth, signOut } from "firebase/auth";
+import { app } from "./firebase/Firebase";
 import "./App.css";
 import { Login } from "./container/Login";
+import { Chat } from "./container/Chat/Chat";
 function App() {
+  const [authUser, setAuthUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setAuthUser(user);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="container">
+        <p>Loading....</p>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
-      <Login />
+      {!!authUser ? <Chat user={authUser} /> : <Login />}
     </div>
   );
 }
