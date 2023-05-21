21aabd7614be62697ee4f88db33df0185149fc71
bukhtyar <bukhtyar.haider1@gmail.com>
2023-05-21 16:35:02 +0500

    added Firestore function to add and fetch data

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,4 +1,13 @@
 import { initializeApp } from "firebase/app";
+import {
+  collection,
+  addDoc,
+  getFirestore,
+  getDocs,
+  query,
+  where,
+  orderBy,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCW-5qHRoeV0N2A17My_jEzolkI3nvd92o",
@@ -10,3 +19,68 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export const addUser = async (user) => {
+  try {
+    const userRef = await addDoc(collection(db, "users"), user);
+    console.log(userRef.id);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getUsers = async (currentUserId) => {
+  try {
+    const userSnapshot = await getDocs(collection(db, "users"));
+    const usersData = [];
+
+    userSnapshot.forEach((doc) => {
+      if (currentUserId !== doc.data().id) usersData.push(doc.data());
+    });
+
+    return usersData;
+  } catch (error) {
+    console.log("Error getUsers:", error);
+  }
+};
+
+export const sendMessage = async (senderId, receiverId, message) => {
+  const newMessage = {
+    senderId,
+    receiverId,
+    message,
+    timestamp: new Date().getTime(),
+  };
+
+  try {
+    const messageRef = await addDoc(collection(db, "Messages"), newMessage);
+    return { ...newMessage, id: messageRef.id };
+  } catch (error) {
+    console.log(`Error sendMessage:${error}`);
+  }
+};
+
+export const getChatMessages = async (senderId, receiverId) => {
+  try {
+    const messagesQuery = query(
+      collection(db, "Messages"),
+      where("senderId", "in", [senderId, receiverId]),
+      where("receiverId", "in", [senderId, receiverId]),
+      orderBy("timestamp", "asc")
+    );
+
+    const querySnapshot = await getDocs(messagesQuery);
+
+    const messages = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    return messages;
+  } catch (error) {
+    console.error("Error retrieving messages:", error);
+    return [];
+  }
+};
